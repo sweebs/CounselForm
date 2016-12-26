a95237ca13b5ea46c74224a587968d6ff097ac29
Paul Sweby <Paul Sweby>
2016-12-26 11:12:45 +0000

    Added Button to link to Web Site

--- app/src/main/java/uk/co/sweby/counselform/MainActivity.java
+++ app/src/main/java/uk/co/sweby/counselform/MainActivity.java
@@ -1,13 +1,19 @@
 package uk.co.sweby.counselform;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -25,6 +31,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     public static String currentPointOfCounsel;
 
     public static String dateAssigned;
+
+    public static String url;
 
 
     @Override
@@ -45,10 +54,7 @@ public class MainActivity extends AppCompatActivity {
             currentPointOfCounsel = item.getPointOfCounsel();
             dateAssigned = item.getDateSet();
             setFields();
-        } /* else if (savedInstanceState != null) {
-            dateAssigned = savedInstanceState.getString("dateAssigned");
-            currentPointOfCounsel = savedInstanceState.getString("currentPointOfCounsel");
-        } */
+        }
     }
 
     @Override
@@ -64,12 +70,28 @@ public class MainActivity extends AppCompatActivity {
         displayPoc.setText(currentPointOfCounsel);
         TextView displayDate = (TextView) findViewById(R.id.dateAssigned);
         displayDate.setText(dateAssigned);
+        Resources res = getResources();
+        StringTokenizer str = new StringTokenizer(currentPointOfCounsel, " ");
+        str.nextToken();
+        String index = str.nextToken();
+        int i = Integer.parseInt(index);
+        String[] urls = res.getStringArray(R.array.urls);
+        url = urls[i-1];
+        Button button = (Button) findViewById(R.id.button_url);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.home:
             case R.id.selectPointOfCounsel:
                 launchSelectPointOfCounsel();
                 return true;
